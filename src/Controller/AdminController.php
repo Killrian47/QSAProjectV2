9f789ad45ac2e23ef44333b79b2cba3ae617b3eb
salut <killian.portier@yahoo.fr>
2023-04-03 12:50:21 +0200

    admin view

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,30 +4,63 @@ namespace App\Controller;
 
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    /**
+     * @throws NonUniqueResultException
+     * @throws NoResultException
+     */
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-
+        $totalEntreprise = $entrepriseRepository->createQueryBuilder('e')
+            ->select(['count(e.id)'])
+            ->getQuery()
+            ->getSingleScalarResult();
 
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'totalEntreprise' => $totalEntreprise
         ]);
     }
 
-    #[Route('/admin/{letter}', name: 'app_start')]
+    #[Route('/admin/{letter}', name: 'app_entreprise')]
     public function firstLetter(string $letter, EntrepriseRepository $entrepriseRepository): Response
     {
         $entreprises = $entrepriseRepository->findBy([], ['name' => 'ASC']);
+        $entreprisesStartByA = [];
+        $entreprisesStartByB = [];
+        $entreprisesStartByC = [];
+        $entreprisesStartByD = [];
+        $entreprisesStartByE = [];
+        $entreprisesStartByF = [];
+        $entreprisesStartByG = [];
+        $entreprisesStartByH = [];
+        $entreprisesStartByI = [];
+        $entreprisesStartByJ = [];
+        $entreprisesStartByK = [];
+        $entreprisesStartByL = [];
+        $entreprisesStartByM = [];
+        $entreprisesStartByN = [];
+        $entreprisesStartByO = [];
+        $entreprisesStartByP = [];
+        $entreprisesStartByQ = [];
+        $entreprisesStartByR = [];
+        $entreprisesStartByS = [];
+        $entreprisesStartByT = [];
+        $entreprisesStartByU = [];
+        $entreprisesStartByV = [];
+        $entreprisesStartByW = [];
+        $entreprisesStartByX = [];
+        $entreprisesStartByY = [];
+        $entreprisesStartByZ = [];
         if ($letter === 'ABC' || $letter === 'abc') {
-            $entreprisesStartByA = [];
-            $entreprisesStartByB = [];
-            $entreprisesStartByC = [];
             foreach ($entreprises as $value) {
                 for ($i = 0; $i < 1; $i++) {
                     if ($value->getName()[$i] === 'a' || $value->getName()[$i] === 'A') {
@@ -39,10 +72,12 @@ class AdminController extends AbstractController
                     }
                 }
             }
+            return $this->render('admin/all_entreprise.html.twig', [
+                'entreprisesStartByA' => $entreprisesStartByA,
+                'entreprisesStartByB' => $entreprisesStartByB,
+                'entreprisesStartByC' => $entreprisesStartByC,
+            ]);
         } elseif ($letter === 'DEF' || $letter === 'def') {
-            $entreprisesStartByD = [];
-            $entreprisesStartByE = [];
-            $entreprisesStartByF = [];
             foreach ($entreprises as $value) {
                 for ($i = 0; $i < 1; $i++) {
                     if ($value->getName()[$i] === 'd' || $value->getName()[$i] === 'F') {
@@ -54,110 +89,93 @@ class AdminController extends AbstractController
                     }
                 }
             }
+            return $this->render('admin/all_entreprise.html.twig', [
+                'entreprisesStartByD' => $entreprisesStartByD,
+                'entreprisesStartByE' => $entreprisesStartByE,
+                'entreprisesStartByF' => $entreprisesStartByF,
+            ]);
         } elseif ($letter === 'GHIJ' || $letter === 'ghij') {
-            $entreprisesStartByG = [];
-            $entreprisesStartByH = [];
-            $entreprisesStartByI = [];
-            $entreprisesStartByJ = [];
             foreach ($entreprises as $value) {
                 for ($i = 0; $i < 1; $i++) {
                     if ($value->getName()[$i] === 'g' || $value->getName()[$i] === 'G') {
-                        $entreprisesStartByD[] = $value;
+                        $entreprisesStartByG[] = $value;
                     } elseif ($value->getName()[$i] === 'h' || $value->getName()[$i] === 'H') {
-                        $entreprisesStartByE[] = $value;
+                        $entreprisesStartByH[] = $value;
                     } elseif ($value->getName()[$i] === 'i' || $value->getName()[$i] === 'I') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByI[] = $value;
                     } elseif ($value->getName()[$i] === 'j' || $value->getName()[$i] === 'J') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByJ[] = $value;
                     }
                 }
             }
+            return $this->render('admin/all_entreprise.html.twig', [
+                'entreprisesStartByG' => $entreprisesStartByG,
+                'entreprisesStartByH' => $entreprisesStartByH,
+                'entreprisesStartByI' => $entreprisesStartByI,
+                'entreprisesStartByJ' => $entreprisesStartByJ,
+            ]);
         } elseif ($letter === 'KLM' || $letter === 'klm') {
-            $entreprisesStartByK = [];
-            $entreprisesStartByL = [];
-            $entreprisesStartByM = [];
             foreach ($entreprises as $value) {
                 for ($i = 0; $i < 1; $i++) {
                     if ($value->getName()[$i] === 'k' || $value->getName()[$i] === 'K') {
-                        $entreprisesStartByD[] = $value;
+                        $entreprisesStartByK[] = $value;
                     } elseif ($value->getName()[$i] === 'l' || $value->getName()[$i] === 'L') {
-                        $entreprisesStartByE[] = $value;
+                        $entreprisesStartByL[] = $value;
                     } elseif ($value->getName()[$i] === 'm' || $value->getName()[$i] === 'M') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByM[] = $value;
                     }
                 }
             }
         } elseif ($letter === 'NOPQ' || $letter === 'nopq') {
-            $entreprisesStartByN = [];
-            $entreprisesStartByO = [];
-            $entreprisesStartByP = [];
-            $entreprisesStartByQ = [];
             foreach ($entreprises as $value) {
                 for ($i = 0; $i < 1; $i++) {
                     if ($value->getName()[$i] === 'n' || $value->getName()[$i] === 'N') {
-                        $entreprisesStartByD[] = $value;
+                        $entreprisesStartByN[] = $value;
                     } elseif ($value->getName()[$i] === 'o' || $value->getName()[$i] === 'O') {
-                        $entreprisesStartByE[] = $value;
+                        $entreprisesStartByO[] = $value;
                     } elseif ($value->getName()[$i] === 'p' || $value->getName()[$i] === 'P') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByP[] = $value;
                     } elseif ($value->getName()[$i] === 'q' || $value->getName()[$i] === 'Q') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByQ[] = $value;
                     }
                 }
             }
         } elseif ($letter === 'RSTU' || $letter === 'rstu') {
-            $entreprisesStartByR = [];
-            $entreprisesStartByS = [];
-            $entreprisesStartByT = [];
-            $entreprisesStartByU = [];
             foreach ($entreprises as $value) {
                 for ($i = 0; $i < 1; $i++) {
                     if ($value->getName()[$i] === 'r' || $value->getName()[$i] === 'R') {
-                        $entreprisesStartByD[] = $value;
+                        $entreprisesStartByR[] = $value;
                     } elseif ($value->getName()[$i] === 's' || $value->getName()[$i] === 'S') {
-                        $entreprisesStartByE[] = $value;
+                        $entreprisesStartByS[] = $value;
                     } elseif ($value->getName()[$i] === 't' || $value->getName()[$i] === 'T') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByT[] = $value;
                     } elseif ($value->getName()[$i] === 'u' || $value->getName()[$i] === 'U') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByU[] = $value;
                     }
                 }
             }
         } elseif ($letter === 'VWXYZ' || $letter === 'vwxyz') {
-            $entreprisesStartByV = [];
-            $entreprisesStartByW = [];
-            $entreprisesStartByX = [];
-            $entreprisesStartByY = [];
-            $entreprisesStartByZ = [];
             foreach ($entreprises as $value) {
                 for ($i = 0; $i < 1; $i++) {
                     if ($value->getName()[$i] === 'v' || $value->getName()[$i] === 'V') {
-                        $entreprisesStartByD[] = $value;
+                        $entreprisesStartByV[] = $value;
                     } elseif ($value->getName()[$i] === 'w' || $value->getName()[$i] === 'W') {
-                        $entreprisesStartByE[] = $value;
+                        $entreprisesStartByW[] = $value;
                     } elseif ($value->getName()[$i] === 'x' || $value->getName()[$i] === 'X') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByX[] = $value;
                     } elseif ($value->getName()[$i] === 'y' || $value->getName()[$i] === 'Y') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByY[] = $value;
                     } elseif ($value->getName()[$i] === 'z' || $value->getName()[$i] === 'Z') {
-                        $entreprisesStartByF[] = $value;
+                        $entreprisesStartByZ[] = $value;
                     }
                 }
             }
         }
 
-        return $this->render('admin/test.html.twig', [
+        return $this->render('admin/all_entreprise.html.twig', [
             'controller_name' => 'AdminController',
-            'entreprisesStartByA' => $entreprisesStartByA,
-            'entreprisesStartByB' => $entreprisesStartByB,
-            'entreprisesStartByC' => $entreprisesStartByC,
-            'entreprisesStartByD' => $entreprisesStartByD,
-            'entreprisesStartByE' => $entreprisesStartByE,
-            'entreprisesStartByF' => $entreprisesStartByF,
-            'entreprisesStartByG' => $entreprisesStartByG,
-            'entreprisesStartByH' => $entreprisesStartByH,
-            'entreprisesStartByI' => $entreprisesStartByI,
-            'entreprisesStartByJ' => $entreprisesStartByJ,
+
+
             'entreprisesStartByK' => $entreprisesStartByK,
             'entreprisesStartByL' => $entreprisesStartByL,
             'entreprisesStartByM' => $entreprisesStartByM,
