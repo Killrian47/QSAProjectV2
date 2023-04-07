663960a08e8cdad866de7e949a0dc49462a64ba9
salut <killian.portier@yahoo.fr>
2023-04-07 08:49:57 +0200

    fix

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Entreprise;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise/{id}', name: 'app_entreprise')]
-    public function index(): Response
+    public function index(Entreprise $entreprise): Response
     {
+
+
         return $this->render('entreprise/index.html.twig', [
             'controller_name' => 'EntrepriseController',
         ]);
